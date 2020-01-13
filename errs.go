5a428bbb1b43822b39af149dfd54b9f6426fb882
Spiegel <spiegel.im.spiegel@gmail.com>
2020-01-13 13:50:28 +0900

    Update comment

--- errs.go
+++ errs.go
@@ -238,7 +238,7 @@ func encodeJSON(err error) string {
 	return "{" + strings.Join(elms, ",") + "}"
 }
 
-/* Copyright 2019 Spiegel
+/* Copyright 2019,2020 Spiegel
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
--- errs_test.go
+++ errs_test.go
@@ -235,7 +235,7 @@ func TestAs(t *testing.T) {
 	}
 }
 
-/* Copyright 2019 Spiegel
+/* Copyright 2019,2020 Spiegel
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
